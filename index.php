76420164808a3430f1e4f0795d7fbd59e286d42a
webstylecenter <petervandam24@hotmail.com>
2016-01-13 20:36:40 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<html>
+<head>
+    <title>SlideBooker</title>
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script type="text/javascript" src="scripts/main.js"></script>
+    <!-- <meta http-equiv="refresh" content="2"> -->
+</head>
+<body>
+<h1>SlideBooker</h1>
+
+<div class="group">
+    <div class="resizable draggable blue">
+        <h3>Beterhout - BHREM 192</h3>
+        <input type="text" value="Wijziging aangebracht die straks weer eruit mag"/>
+        <div class="startime">10:00</div>
+        <div class="endtime">12:15</div>
+    </div>
+    <div class="resizable draggable blue">
+        <h3>Beterhout - BHREM 192</h3>
+        <input type="text" value="Wijziging aangebracht die straks weer eruit mag"/>
+        <div class="startime">10:00</div>
+        <div class="endtime">12:15</div>
+    </div>
+</div>
+
+<div class="group">
+    <div class="resizable draggable red">
+        <h3>Mira - mira 92</h3>
+        <input type="text" value="Wijziging aangebracht die straks weer eruit mag"/>
+        <div class="startime">10:00</div>
+        <div class="endtime">12:15</div>
+    </div>
+</div>
+
+<div class="resizable draggable orange">
+    <h3>KastenDiscount - KAST 48</h3>
+    <input type="text" value="Wijziging aangebracht die straks weer eruit mag"/>
+    <div class="startime">10:00</div>
+    <div class="endtime">12:15</div>
+</div>
+
+
+<div id="timeline">
+    <div class="hour">
+        <div class="quarter">09:00</div>
+        <div class="quarter">15</div>
+        <div class="quarter">30</div>
+        <div class="quarter">45</div>
+    </div>
+    <div class="hour">
+        <div class="quarter">10:00</div>
+        <div class="quarter">15</div>
+        <div class="quarter">30</div>
+        <div class="quarter">45</div>
+    </div>
+    <div class="hour">
+        <div class="quarter">11:00</div>
+        <div class="quarter">15</div>
+        <div class="quarter">30</div>
+        <div class="quarter">45</div>
+    </div>
+    <div class="hour">
+        <div class="quarter">12:00</div>
+        <div class="quarter break">15</div>
+        <div class="quarter break">30</div>
+        <div class="quarter">45</div>
+    </div>
+    <div class="hour">
+        <div class="quarter">13:00</div>
+        <div class="quarter">15</div>
+        <div class="quarter">30</div>
+        <div class="quarter">45</div>
+    </div>
+    <div class="hour">
+        <div class="quarter">14:00</div>
+        <div class="quarter">15</div>
+        <div class="quarter">30</div>
+        <div class="quarter">45</div>
+    </div>
+    <div class="hour">
+        <div class="quarter">15:00</div>
+        <div class="quarter">15</div>
+        <div class="quarter">30</div>
+        <div class="quarter">45</div>
+    </div>
+    <div class="hour">
+        <div class="quarter">16:00</div>
+        <div class="quarter">15</div>
+        <div class="quarter">30</div>
+        <div class="quarter">45</div>
+    </div>
+    <div class="hour">
+        <div class="quarter">17:00</div>
+        <div class="quarter">15</div>
+        <div class="quarter">30</div>
+        <div class="quarter">45</div>
+    </div>
+</div>
+</body>
+</html>
---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by petervandam on 13/01/16.
+ */
+$(function() {
+    $( ".resizable" ).resizable({
+        grid: 50,
+        maxHeight: 50,
+        minHeight: 50
+    });
+
+    $(".draggable").draggable({
+        snap: ".quarter",
+    });
+
+    $(".quarter").droppable({
+        drop: function(event, ui) {
+            $(this).css('background-color', "whitesmoke");
+        },
+        tolerance: "touch",
+        hoverClass: "active"
+    });
+});
