7dd9d9d69b225e5ab0f834c4acbe9512cef0b1dd
webstylecenter <petervandam24@hotmail.com>
2016-02-07 15:31:03 +0100

    Created booking table

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script type="text/javascript" src="scripts/time.js"></script>
     <script type="text/javascript" src="scripts/interaction.js"></script>
     <script type="text/javascript" src="scripts/gap-filler.js"></script>
-    <!-- <meta http-equiv="refresh" content="2"> -->
+    <meta http-equiv="refresh" content="2">
 </head>
 <body>
 <h1>SlideBooker (Work in Progress)</h1>
@@ -21,63 +21,88 @@
     <li>Schuif de taken hieronder naar de tijdlijn beneden om de opdracht te gaan boeken</li>
     <li>Vervang de omschrijving van de taak door de tekst te selecteren en aan te passen</li>
     <li>Verklein of vergroot de opdracht door aan de randen te trekken</li>
-    <li><a href="javascript:createTask('test', 'addedDescription', 'blue');">Create task</a></li>
     <li><a href="https://github.com/webstylecenter/SlideBooker" target="_blank">Download repository</a></li>
 </ul>
 
-<div id="tasks">
-    <div class="group">
-        <div class="task resizable draggable blue">
-            <h3>Beterhout - BHREM 192</h3>
-            <input type="text" value="Wijziging aangebracht die straks weer eruit mag"/>
-            <div class="startime">-</div>
-            <div class="tasktime">task</div>
-            <div class="endtime">-</div>
-        </div>
-        <div class="task resizable draggable blue">
-            <h3>Beterhout - BHREM 192</h3>
-            <input type="text" value="Wijziging aangebracht die straks weer eruit mag"/>
-            <div class="startime">-</div>
-            <div class="tasktime">-</div>
-            <div class="endtime">-</div>
-        </div>
-    </div>
-    <div class="group">
-        <div class="task resizable draggable red">
-            <h3>Mira - mira 92</h3>
-            <input type="text" value="Wijziging aangebracht die straks weer eruit mag"/>
-            <div class="startime">-</div>
-            <div class="tasktime">-</div>
-            <div class="endtime">-</div>
-        </div>
-    </div>
-    <div class="task resizable draggable orange">
-        <h3>KastenDiscount - KAST 48</h3>
-        <input type="text" value="Wijziging aangebracht die straks weer eruit mag"/>
-        <div class="startime">-</div>
-        <div class="tasktime">-</div>
-        <div class="endtime">-</div>
-    </div>
-</div>
+<form id="booker">
 
-<div id="timeline">
-    <?php
+    <table>
+        <tr>
+            <th>Project</th>
+            <th>Opdracht</th>
+            <th>Starttijd</th>
+            <th>Eindtijd</th>
+            <th>Tijdsduur</th>
+        </tr>
+        <?php
 
-    $hours = array(9, 10, 11, 12, 13, 14, 15, 16, 17);
-    foreach ($hours as $hour) {
-        echo '<div class="hour">
-                <div class="quarter" data-taken="no" data-starttime="'.$hour.':00" data-endtime="'.$hour.':15">'.$hour.':00</div>
-                <div class="quarter" data-taken="no" data-starttime="'.$hour.':15" data-endtime="'.$hour.':30">15</div>
-                <div class="quarter" data-taken="no" data-starttime="'.$hour.':30" data-endtime="'.$hour.':45">30</div>
-                <div class="quarter" data-taken="no" data-starttime="'.$hour.':45" data-endtime="'.($hour+1).':00">45</div>
-            </div>';
-    }
+        for ($i=1; $i<=20; $i++) {
 
-    ?>
-</div>
+            $disabled = ($i == 1 ? '' : ' disabled="disabled"');
+            echo '<tr>
+                    <td>
+                        <select name="project'.$i.'"'.$disabled.'">
+                            <option value="">Maak een keuze</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input type="text" value=""'.$disabled.'" />
+                    </td>
+                    <td>
+                        <select name="starttijd'.$i.'"'.$disabled.'">
+                            <option value="">'.($i == 1 ? '09:00' : '-').'</option>
+                        </select>
+                    </td>
+                    <td>
+                        <select name="project'.$i.'"'.$disabled.'">
+                            <option value="">'.($i == 1 ? '09:15' : '-').'</option>
+                        </select>
+                    </td>
+                    <td>'.($i == 1 ? '00:15' : '').'</td>
+                </tr>';
 
-<div id="randomTasks">
+        }
 
+        ?>
+        <tr>
+            <td>
+                <select name="projectFinal">
+                    <option value="peter">Peter van Dam</option>
+                </select>
+            </td>
+            <td>
+                <input type="text" value="Random stuff" />
+            </td>
+            <td colspan="3">
+                06:00
+            </td>
+        </tr>
+        <tr>
+            <td colspan="5">
+                <input type="button" value="Save" />
+            </td>
+        </tr>
+    </table>
+
+</form>
+
+<div id="progressbar">
+    <table cellspacing="0" cellpadding="0">
+        <tr>
+            <td>09:00</td>
+            <?php
+
+                $time = 0;
+                while($time < 375) {
+                    echo '<td class="'.(rand(1, 3) == 1 ? 'taken' : 'free').'">&nbsp;</td>';
+                    $time=$time+15;
+                }
+
+
+            ?>
+            <td>15:45</td>
+        </tr>
+    </table>
 </div>
 
 </body>
